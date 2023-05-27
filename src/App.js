034e97d9441fa98971fde01d91ab71bea48c7d80
Pham Xuan Viet Cuong <pxvcuong2347@gmail.com>
2023-05-27 17:12:06 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Attribution from "./components/Attribution";
+
+function App() {
+	return (
+		<>
+			Day DD Month MM Year YYYY -- years -- months -- days
+			<Attribution />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Attribution/index.jsx
@@ -0,0 +1,24 @@
+function Attribution() {
+	return (
+		<div className="attribution">
+			Challenge by{" "}
+			<a
+				href="https://frontendmentor.io/challenges"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Frontend Mentor
+			</a>
+			. Coded by{" "}
+			<a
+				href="https://frontendmentor.io/profile/PXVC"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Phạm Xuân Việt Cường
+			</a>
+		</div>
+	);
+}
+
+export default Attribution;
